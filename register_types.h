f43e174e9546982190f090d3ce708840973af221
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-02-04 19:20:55 -0800

    Add material baking
    First commit.
    
    
    Try to link.
    
    
    Add initial stub baker.
    
    
    Remove github workflow.
    
    
    Return a standard material 3d.
    
    
    Load default paths.
    
    
    Now getting baking errors.
    
    
    Trim todo.
    
    
    Use material_x naming.
    
    
    Ban javascript.
    
    
    Merge changes.
    
    
    Restore build.
    
    
    Try better search paths.
    
    
    Scope the xt library.
    
    
    Format code.
    
    
    Restore old.
    
    
    Remove unused code.
    
    
    Use an absolute path.
    
    
    Use new path to save file.
    
    
    Use materialx ext.
    
    
    Create LICENSE
    Print material attributes.
    
    
    Save
    
    
    Checkpoint.
    
    
    Print texture path.
    
    
    Prepare to store the values and categories of a MaterialX Node..
    
    
    Support more material types.
    
    
    Add more mappings.
    
    
    Use the current path to save.
    
    
    Don't recurse save mtlx.
    
    
    Don't use raw string for the base materialx path.
    
    
    Read the materialx twice.
    
    
    Change print to print verbose.
    
    
    Implement more material properties.
    
    
    Checkpoint working better.
    
    
    Save.
    
    
    Revert "Save."
    
    This reverts commit 75aadd566f8cb80373b6890b75d6a9f09161fe33.
    
    Revert "Checkpoint working better."
    
    This reverts commit e959071f36acee121369f961f1bd73572fbe2b3d.
    
    Avoid crashes.
    
    
    Use a static variable to hold materialx.
    
    
    Restore metallic.
    
    
    Set metallic to default to 1.0f.
    
    
    Avoid crashing.
    
    
    Don't repeat the search path code.
    
    
    Add more catch statements.
    
    
    Check the list of gltf properties.
    
    
    Force srgb on albedo.
    
    
    Typo.
    
    
    Scan for the normal map.
    
    
    Guard against invalid data.
    
    
    Emissive property name change.
    
    
    Merge.
    
    
    Emission change.
    
    
    Update get resource type.
    
    
    Separate licensing.
    
    
    Typo.
    
    
    Use the proper materialx.
    
    
    Pause.
    
    
    Restore materialx.
    
    
    Block linux.
    
    
    Fails on all systems.
    
    
    Restore materialx building.
    
    
    Update MaterialX version.
    
    
    Ignore missing braces.
    
    
    Avoid c++ braces warning.
    
    
    Add mac compatibility.
    
    
    Add mac compatibility. Try 2.
    
    
    Disable material to debug exceptions.
    
    
    Update register module.
    
    
    Restore materialx.
    
    
    Update MTLX loader.
    
    
    Restore old type.
    
    
    Make libraries relative to the materialx.
    
    
    Target the source file.
    
    
    Properly remap the mtlx.
    
    
    Order the config settings.
    
    
    Don't generate a new id.
    
    
    Add material remapping.
    
    
    Only generate a uid if it doesn't exist.
    
    
    Disable materialx.
    
    
    Removed submodule source/MaterialXView/NanoGUI
    
    
    Restore.
    
    
    Add godot-cpp.
    
    
    Add gdextension.
    
    
    Commit first draft changes.
    
    
    Name the c api correctly.
    
    
    Remove godot-cpp.
    
    
    Remove godot-cpp.
    
    
    Revert "Merge remote-tracking branch 'origin/godot' into HEAD"
    
    This reverts commit 20cb0e76ffb1cb0a2176250764198104b6976c46.
    
    Save work.
    
    
    Revert "Save work."
    
    This reverts commit 90a4b6a0a6dfb18be0abcbb5fce7c17eda7588fc.
    
    Keep clang.
    
    
    Revert "Merge branch 'godot' into old"
    
    This reverts commit ab0469b823a56528f5153745328774e159a1c682.
    
    Remove unused code.
    
    
    Removed submodule godot-cpp
    Remove unused.
    
    
    Remove godot .cpp.
    
    
    Add a materialx plugin.
    
    
    Restore gdextension.
    
    
    Add godot c++.
    
    
    Restore materialx as a module.
    
    
    Restore working.
    
    
    Merge materialx.
    
    
    Return true.
    
    
    Move output.
    
    
    Save as resource basename and .res.
    
    
    This is an open file dialog.
    
    
    Filter to resources.
    
    
    Globalize path.
    
    
    Loader runs earlier.
    
    Path issues.
    Avoid opengl crash.
    
    
    Get base dir.
    
    
    Cleanup default search paths.
    
    
    Remove MaterialXPlugin.
    
    
    Paths are hard.
    
    
    Support full paths.
    
    
    Use imported folder.
    
    
    Use importer paths.
    
    
    Revert "Use importer paths."
    
    This reverts commit 54e4a7a9f4b71a584216a3ca3a458f962ac12e36.
    
    Revert "Use imported folder."
    
    This reverts commit e724d3d943ba8265596e80b7635599157a253d50.
    
    Update defines for linux.
    
    
    Use the scene level.
    
    
    Appease the compiler.

---
+++ register_types.h
@@ -0,0 +1,9 @@
+#ifndef REGISTER_MATERIALX_H
+#define REGISTER_MATERIALX_H
+
+#include "modules/register_module_types.h"
+
+void initialize_material_x_module(ModuleInitializationLevel p_level);
+void uninitialize_material_x_module(ModuleInitializationLevel p_level);
+
+#endif
